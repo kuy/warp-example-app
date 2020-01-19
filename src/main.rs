cde5d8946c4e826c0f638c3f330ed52bd4738225
Yuki Kodama <endflow.net@gmail.com>
2020-01-19 10:30:38 +0900

    Support tera template

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use std::convert::Infallible;
-use warp::{self, path, Filter};
+use tera::Tera;
+use warp::{self, filters::BoxedFilter, path, Filter};
 
-async fn hello(name: String) -> Result<impl warp::Reply, Infallible> {
-    Ok(format!("Hello, {}!", name))
+pub fn create_template_filter() -> BoxedFilter<(Tera,)> {
+    let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/*.html")).unwrap();
+    warp::any().map(move || tera.clone()).boxed()
+}
+
+async fn hello(name: String, tmpl: Tera) -> Result<impl warp::Reply, Infallible> {
+    let mut ctx = tera::Context::new();
+    ctx.insert("name", &name);
+    let payload = tmpl.render("hello.html", &ctx).unwrap();
+    Ok(warp::reply::html(payload))
 }
 
 #[tokio::main]
 async fn main() {
-    let routes = path!("hello" / String).and_then(hello);
+    let tmpl = create_template_filter();
+    let routes = path!("hello" / String).and(tmpl).and_then(hello);
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
